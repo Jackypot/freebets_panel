d26740f8b9e6ae5493785d4326bd3cf82246e12d
Jesus Glez <angelotr12@hotmail.com>
2018-03-29 12:28:30 -0600

    se agregaron los endpoints a operativo1 ya con su funcionamiento correcto

--- js/main.js
+++ js/main.js
@@ -32,17 +32,28 @@ class Btn_Swich {
 	new Btn_Swich();
 })();
 
-
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Funcion de post - Recibir datos
+// Calcular tiros gratis
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function recibirDatos (NumOperativo){
+function calcularTiros (valor){
+    let valor_a_calcular = parseInt(valor) / 1000000000000000000;
+    let tiros_gratis = valor_a_calcular / 0.01;
 
+    return [tiros_gratis, valor_a_calcular];
+}
+
+var OperativoUnoActivo = false;
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Funcion de post - Recibir datos Operativo 1
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function recibirDatosOP1(){
     var DatosR = new Promise(function (resolve, reject) {
+        let url = "https://jackynet.eu-4.evennode.com/freebets/OP1";
 
-        let url = "http://192.168.1.75:8080/freebets";
-        return fetch(url, {method: 'post', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, body: JSON.stringify({ "operativo": parseInt(NumOperativo) }) })
-        .then(function(response){if (!response.ok) {throw Error(response.statusText);} resolve(response.json());})
+        return fetch(url, {method: 'post', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, body: JSON.stringify({ }) })
+        .then(function(response){
+            if (!response.ok) {throw Error(response.statusText);}
+            resolve(response.json());})
         .catch(function(error) { console.error('Parece que hubo un error: ' + error); });
 
         reject(new Error('Parece que tuvimos un error'))
@@ -51,22 +62,7 @@ function recibirDatos (NumOperativo){
 
     return DatosR;
 }
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Calcular tiros gratis
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function calcularTiros (valor){
-    let valor_a_calcular = parseInt(valor) / 1000000000000000000;
-    let tiros_gratis = valor_a_calcular / 0.01;
-
-    return tiros_gratis;
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Operativo 1 - Eventos
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-var OperativoUnoActivo = false;
-
 //Funcion de evento CLICK a la navegacion de Operativo 1
 document.getElementById("operativo_1").addEventListener("click", mostrar_datos_Operativo1, false);
 document.getElementById("operativo_1_m").addEventListener("click", mostrar_datos_Operativo1, false);
@@ -80,82 +76,85 @@ function mostrar_datos_Operativo1 (){
     let tabla_operativo1 = document.querySelector(".contenedor_tabla_operativo_1 tbody");
 
     if(tabla_operativo1.childNodes.length > 1) tabla_operativo1.innerHTML = "";
+ 
+    recibirDatosOP1().then(function(data){
+       
+        for (var item in data) {
+            
+            let tr = document.createElement("tr");
+            let id = document.createElement("td");
+            let bets = document.createElement("td");
+            let address = document.createElement("td");
+            let url = document.createElement("td");
+            let check = document.createElement("td");
+            let apuesta = document.createElement("td");
 
-    for (var i = 0; i < 10; i++) {
-
-        let tr = document.createElement("tr");
-        let id = document.createElement("td");
-        let bets = document.createElement("td");
-        let address = document.createElement("td");
-        let url = document.createElement("td");
-        let check = document.createElement("td");
-        let apuesta = document.createElement("td");
-
-        id.innerHTML = i + 1;
-        address.innerHTML = "0xFeac34425a3Ba2FAfbbEEDB367aC5F4b4bB701D2";
-        bets.innerHTML = "1";
-        url.innerHTML = "<input type='text' name='' value=''>"
-        check.innerHTML = "<p><label><input type='checkbox'/><span></span></label></p>"
-        apuesta.innerHTML = "1 ETH";
-
-
-        tr.appendChild(id);
-        tr.appendChild(address);
-        tr.appendChild(apuesta);
-        tr.appendChild(bets);
-        tr.appendChild(url);
-        tr.appendChild(check);
-
-        tr.id = "data-"+i.toString();
-
-        tabla_operativo1.appendChild(tr);
-    }
-    
-    // exiten_datos = true;
-    // eventoOneActivo = true;
-
-    // recibirDatos(1).then(function(data){
-
-    //     for (var item in data) {
-    //         let tabla_datos = document.getElementById("tabla-datos").getElementsByTagName('tbody')[0];
-    //         let tr = document.createElement("tr");
-    //         let id = document.createElement("td");
-    //         let bets = document.createElement("td");
-    //         let address = document.createElement("td");
-    //         let url = document.createElement("td");
-    //         let check = document.createElement("td");
-    //         let apuesta = document.createElement("td");
-
-    //         id.innerHTML = data[item].id;
-    //         address.innerHTML = data[item].address;
-
-    //         let tiros_correspondientes = calcularTiros(data[item].Valor);
-
-    //         bets.innerHTML = tiros_correspondientes;
-    //         url.innerHTML = "<input type='text' name='' value=''>"
-    //         check.innerHTML = "<p><label><input type='checkbox'/><span></span></label></p>"
-    //         apuesta.innerHTML = (data[item].Valor / 100000000000000000).toString()+" ETH";
+            id.innerHTML = data[item].id;
+            address.innerHTML = data[item].address;
+            let aux = calcularTiros(parseInt(data[item].Valor));
+            apuesta.innerHTML = aux[1];
+            bets.innerHTML = aux[0];
+            url.innerHTML = "<input type='text' name='' value=''>"
+            check.innerHTML = "<p><label><input type='checkbox'/><span></span></label></p>"
 
 
-    //         tr.appendChild(id);
-    //         tr.appendChild(address);
-    //         tr.appendChild(apuesta);
-    //         tr.appendChild(bets);
-    //         tr.appendChild(url);
-    //         tr.appendChild(check);
+            tr.appendChild(id);
+            tr.appendChild(address);
+            tr.appendChild(apuesta);
+            tr.appendChild(bets);
+            tr.appendChild(url);
+            tr.appendChild(check);
 
-    //         tr.id = "data-"+item.toString();
+            tr.id = "data-"+item.toString();
 
-    //         tabla_datos.appendChild(tr);
-    //     }
+            tabla_operativo1.appendChild(tr);
+        }
 
-    // });
+    });
 
 
 }
+//Verificacion de datos antes de enviar
+document.getElementById("btnEnvioOp1").addEventListener("click", checkDatosOP1, false);
+function checkDatosOP1 (){
+    M.Toast.dismissAll();
+        let checkboxes = document.querySelector("#tabla_operativo_1 tbody").getElementsByTagName('input');
+        let contador = 1;
+        let array = [];
+
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].type === 'checkbox') {
+                if(checkboxes[i].checked === true){
+
+                    let dato_guardar = document.getElementById("data-"+(i-contador).toString());
+                    let aux = {"id": dato_guardar.childNodes[0].innerText, "folio": "bitcointalkPromo", "numTiros": dato_guardar.childNodes[2].innerText, "url": dato_guardar.childNodes[4].childNodes[0].value};
+                    array.push(aux);
+
+                };
+                contador ++;
+            }
+        }
+
+        if (array.length > 0) {
+            console.log(array);
+            
+            enviar_datos_eventoOne(array);
+        }else { M.toast({html: 'No se estan enviando datos, porfavor seleccione datos a enviar', classes: 'rounded'}); }
+}
+//Envio de datos
+function enviar_datos_eventoOne(json){
+    let url = "https://jackynet.eu-4.evennode.com/freebets/OP1";
+    return fetch(url, {method: 'post', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, body: JSON.stringify({"data":json })})
+    .then(function(response){
+        if (!response.ok) {throw Error(response.statusText);}
+        M.toast({html: 'Datos enviados con exito', classes: 'rounded'});
+        mostrar_datos_Operativo1();
+    })
+    .catch(function(error) { console.error('Parece que hubo un error: ' + error); });
+}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Operativo 2 - Eventos
+// Funcion de post - Recibir datos Operativo 2
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 document.getElementById("operativo_2").addEventListener("click", accion_Switches, false);
 document.getElementById("operativo_2_m").addEventListener("click", accion_Switches, false);
@@ -273,40 +272,17 @@ function mostrar_datos_demo(){
     }
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Envio de datos - POST
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// document.getElementById("btn-enviar").addEventListener("click", checkDatos, false);
-function checkDatos (){
-    M.Toast.dismissAll();
-    if (eventoOneActivo) {
-        let checkboxes = document.querySelector("#tabla-datos tbody").getElementsByTagName('input');
-        let contador = 1;
-        let array = [];
+function recibirDatosOP2(){
+    var DatosR = new Promise(function (resolve, reject) {
+        let url = "http://192.168.1.75:8080/freebets/OP2";
 
-        for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].type === 'checkbox') {
-                if(checkboxes[i].checked === true){
+        return fetch(url, {method: 'post', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, body: JSON.stringify({ }) })
+        .then(function(response){if (!response.ok) {throw Error(response.statusText);} resolve(response.json());})
+        .catch(function(error) { console.error('Parece que hubo un error: ' + error); });
 
-                    let dato_guardar = document.getElementById("data-"+(i-contador).toString());
-                    let aux = {"id": dato_guardar.childNodes[0].innerText, "folio": "bitcointalkPromo", "numTiros": dato_guardar.childNodes[2].innerText, "url": dato_guardar.childNodes[3].childNodes[0].value};
-                    array.push(aux);
+        reject(new Error('Parece que tuvimos un error'))
 
-                };
-                contador ++;
-            }
-        }
+    });
 
-        if (array.length > 0) {
-            enviar_datos_eventoOne(array);
-        }else { M.toast({html: 'No se estan enviando datos, porfavor seleccione datos a enviar', classes: 'rounded'}); }
-    }else { M.toast({html: 'Parece que estamos en el evento 2', classes: 'rounded'}); }
-}
-
-
-function enviar_datos_eventoOne(json){
-    let url = "http://192.168.1.75:8080/freebets";
-    return fetch(url, {method: 'post', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, body: JSON.stringify({"operativo": 1,"data":json })})
-    .then(function(response){if (!response.ok) {throw Error(response.statusText);} M.toast({html: 'Datos enviados con exito', classes: 'rounded'})})
-    .catch(function(error) { console.error('Parece que hubo un error: ' + error); });
+    return DatosR;
 }
